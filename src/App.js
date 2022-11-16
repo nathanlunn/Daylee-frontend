92ba95b8e26a60b3d7dd1b835ad63002f030d5c9
Summer <nathandouglaslunn@gmail.com>
2022-11-16 09:03:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import PostHome from './components/PostHome.js';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <PostHome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostHome.js
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+export default function PostHome() {
+  const [todaysPost, setTodaysPost] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/posts')
+    .then(res => {
+      setTodaysPost(res.data);
+    })
+    .catch(err => {
+      console.error(err.message);
+    })
+  }, [])
+
+  return (
+    <div>
+      {todaysPost.description}
+    </div>
+  )
+}
