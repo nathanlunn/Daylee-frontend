0b8dcf70a40729f33b5936013c5f016f37cce6d2
Summer <nathandouglaslunn@gmail.com>
2022-11-17 14:23:04 -0500

    use axios.defaults.baseURL instead of proxy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://daylee-backend.onrender.com";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
